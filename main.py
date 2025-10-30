12cf4ad6619df4b373cabb3c52a83dc8005ca93f
Soham Vyas <sohamvyasasia@gmail.com>
2025-10-30 14:25:00 +0530

    Main.py
    Connect to MySQL database and fetch user data.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import mysql.connector
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="admin",
+    database="pythonbatch"
+)
+cursor = connection.cursor()
+cursor.execute("SELECT name, mob, gmail FROM user")
+while True:
+    n = input("How many rows do you want to print  ")
+    if n.strip() == "":
+        print("Over")
+        break
+    n = int(n)
+    if n <= 0:
+        print("enter positive value ")
+    rows = cursor.fetchmany(n)
+    if not rows:
+        print("No more data ")
+        break
+    for x in rows:
+        id , name, mob, gmail = x
+        print(f"id : {id} , name : {name}, mob : {mob}, gmail : {gmail}")
+cursor.close()
+connection.close()
